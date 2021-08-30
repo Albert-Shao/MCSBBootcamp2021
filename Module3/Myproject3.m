87a07ea7ea8afff7212e2aa54c946e83f33204bb
Albert-Shao <wshao04@gmail.com>
2021-08-30 14:21:38 -0700

    Finished Module3

--- Module3/Myproject3.m
+++ Module3/Myproject3.m
@@ -12,6 +12,19 @@ for n = 1:200
     plot(x, '-ok');
 end
 
+figure(1);
+hold on
+
+for r=0:0.2:0.3
+    
+    for n=1:200
+        x(n+1) = x(n) + r.*(1-x(n)/K).*x(n);
+        if n > 100
+            plot(r, x(n), '-ok')
+            hold on
+        end
+    end
+end
+    
 
                                                                     
-for 
