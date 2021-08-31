22209b8e4b2d1fbd317b587ef356f9eeba86b140
Albert-Shao <wshao04@gmail.com>
2021-08-30 18:03:54 -0700

    Save what did on Monday's class
    need to finalize project5

--- Module4/myProject4.m
+++ Module4/myProject4.m
@@ -54,3 +54,31 @@ dPdt =@(P) (a_0 + a_1*P) - b*P;
 plot(T,P)
 ylabel('Molecules of protein A');
 xlabel('Time (hours)');
+
+% parameters
+a = 0;
+b = 0;
+d = 0;
+c = 0;
+
+% model equations
+f =@(x,y) a*x + b*y; 
+g =@(x,y) c*x + d*y;
+
+[T, X] = ode45(@(t,x)[f(x(1),x(2));g(x(1),x(2))], [0,1000], [.1,.1] );
+
+figure; hold on;
+set(gca, 'xlim', [-1, 1], 'ylim', [-1, 1])
+ylabel('x');
+xlabel('y')
+
+xArray = linspace(-1,1,16);
+yArray = linspace(-1,1,16);
+
+[xMesh,yMesh] = meshgrid(xArray, yArray);
+
+% the Matlab plot command for a field of arrows is:
+quiver(xMesh, yMesh, f(xMesh, yMesh), g(xMesh,yMesh))
+
+plot(X(:,1),X(:,2),'-r')
+plot(X(end,1),X(end,2), 'or')
